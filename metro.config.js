4574845d7a48000725577b438b37e403e9bf9682
NS-BOBBY-C <bobby@nextstep.world>
2022-02-02 07:00:14 -0500

    iOS app builds to my iPhone 11
    `expo prebuild --clean -p ios --no-install` worked like a charm and I
    was able to open xcode and build/run the app to my phone.
    
    I had to update the metro.config.js akin to the regular starter-kit
    metro.config.js.

--- metro.config.js
+++ metro.config.js
@@ -1,4 +1,35 @@
 // Learn more https://docs.expo.io/guides/customizing-metro
-const { getDefaultConfig } = require('expo/metro-config');
+const { getDefaultConfig } = require("expo/metro-config");
 
-module.exports = getDefaultConfig(__dirname);
+module.exports = (async () => {
+  const {
+    resolver: { assetExts },
+  } = await getDefaultConfig(__dirname);
+
+  return {
+    transformer: {
+      getTransformOptions: async () => ({
+        transform: {
+          experimentalImportSupport: false,
+          inlineRequires: true,
+        },
+      }),
+    },
+    resolver: {
+      assetExts: [
+        ...assetExts,
+        "obj",
+        "mtl",
+        "mp3",
+        "JPG",
+        "vrx",
+        "hdr",
+        "gltf",
+        "glb",
+        "bin",
+        "arobject",
+        "gif",
+      ],
+    },
+  };
+})();
