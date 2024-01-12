88f7b9125f765b4f488f3fcaaf36fa228e8957a5
robertjcolley <robertcolley04@gmail.com>
2024-01-12 16:23:52 -0500

    Successfully send to eas build

--- metro.config.js
+++ metro.config.js
@@ -1,35 +1,31 @@
 // Learn more https://docs.expo.io/guides/customizing-metro
 const { getDefaultConfig } = require("expo/metro-config");
 
-module.exports = (async () => {
-  const {
-    resolver: { assetExts },
-  } = await getDefaultConfig(__dirname);
+const fileTypes = [
+  "obj",
+  "mtl",
+  "mp3",
+  "JPG",
+  "vrx",
+  "hdr",
+  "gltf",
+  "glb",
+  "bin",
+  "arobject",
+  "gif",
+];
 
-  return {
-    transformer: {
-      getTransformOptions: async () => ({
-        transform: {
-          experimentalImportSupport: false,
-          inlineRequires: true,
-        },
-      }),
-    },
-    resolver: {
-      assetExts: [
-        ...assetExts,
-        "obj",
-        "mtl",
-        "mp3",
-        "JPG",
-        "vrx",
-        "hdr",
-        "gltf",
-        "glb",
-        "bin",
-        "arobject",
-        "gif",
-      ],
-    },
-  };
-})();
+const config = getDefaultConfig(__dirname);
+
+for (const fileType of fileTypes) {
+  config.resolver.assetExts.push(fileType);
+}
+
+config.transformer.getTransformOptions = async () => ({
+  transform: {
+    experimentalImportSupport: false,
+    inlineRequires: true,
+  },
+});
+
+module.exports = config;
